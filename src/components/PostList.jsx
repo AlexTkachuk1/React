e09385c771ea4594f99167e492727ff1dd6057d0
AlexTkachuk1 <Lyucifer97@list.ru>
2022-03-30 23:26:41 +0300

    Тест

---
+++ src/components/PostList.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+   CSSTransition,
+   TransitionGroup,
+ } from 'react-transition-group';
+import Postitem from "./Postitem";
+
+const PostList =({posts, title, remove}) =>{
+
+   if(!posts.length){
+      return <h1>Посты небыли найдены</h1>
+   }
+
+   return (
+      <div>
+        <h1 style={{textAlign: "center"}}>
+            {title}
+        </h1>
+        <TransitionGroup>
+            {posts.map((post, index) =>
+               <CSSTransition
+               key={post.id}
+               timeout={500}
+               classNames="post"
+               >
+               <  Postitem remove={remove} number={index+1} post={post}/>
+               </CSSTransition>
+            )}
+        </TransitionGroup>
+      </div>
+   );
+};
+
+export default PostList;
